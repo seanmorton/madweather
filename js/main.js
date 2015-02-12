ae417ca1d7514eaa2e3b04092041c3e4200c4406
megus <smmorton@wisc.edu>
2015-02-12 01:56:28 -0600

    Change icons to work with Wunderground API

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function getCurrentConditions() {
 
   $.getJSON(url, function(json) {
     var loc = json.current_observation.display_location.full 
-    var icon = json.current_observation.icon_url
+    var icon = "./images/icons/" + json.current_observation.icon + ".png";
     var temp = Math.round(json.current_observation.temp_f) + "&degF"; 
     var desc = json.current_observation.weather
     var wind = json.current_observation.wind_dir + " " +  Math.round(json.current_observation.wind_mph) + "/" + Math.round(json.current_observation.wind_gust_mph) + " MPH";
@@ -40,7 +40,7 @@ function getForecast() {
   $.getJSON(url, function(json) {
     for (i = 0; i < 4; i++) {
       var day = json.forecast.simpleforecast.forecastday[i].date.weekday;
-      var icon = json.forecast.simpleforecast.forecastday[i].icon_url;
+      var icon = "./images/icons/" + json.forecast.simpleforecast.forecastday[i].icon + ".png";
       var high = Math.round(json.forecast.simpleforecast.forecastday[i].high.fahrenheit) + "&degF";
       var low = Math.round(json.forecast.simpleforecast.forecastday[i].low.fahrenheit) + "&degF";
       var desc = json.forecast.simpleforecast.forecastday[i].conditions;
